7ae095cda9eb55d842befa72bb61d181eed9c04e
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-06-22 21:18:47 +0100

    Add code

--- CountdownClock/Program.cs
+++ CountdownClock/Program.cs
@@ -1,1 +1,27 @@
+// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design.Serialization;
+using System.Runtime.InteropServices;
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+
+countdown(10);
+
+static void countdown(int seconds)
+{
+
+    while (seconds >= 0)
+    {
+
+        Console.WriteLine(seconds.ToString());
+
+        seconds -= 1;
+
+        Thread.Sleep(1000);
+
+    }
+
+    Console.WriteLine("Blast off!");
+}               
 
